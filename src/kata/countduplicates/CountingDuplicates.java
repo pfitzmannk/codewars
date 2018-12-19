a4b0710e0ec857d7d24850755e32b914eed75208
Kai Pfitzmann <kai.pfitzmann@exorbyte.com>
2018-12-19 15:17:22 +0100

    1st Solution

--- src/kata/countduplicates/CountingDuplicates.java
+++ src/kata/countduplicates/CountingDuplicates.java
@@ -1,9 +1,28 @@
 package kata.countduplicates;
 
+import java.util.HashSet;
+
 public class CountingDuplicates {
 	  public static int duplicateCount(String text) {
-	    // Write your code here
 		  
-		return 0;
+		  text = text.toLowerCase();
+		  System.out.println(text);
+		  HashSet<String> set = new HashSet<String>();
+		  
+		  
+		  for (int i = 0; i<text.length()-1; i++) {
+			  for (int j=i+1; j<text.length(); j++) {
+				  if (text.charAt(i) == text.charAt(j)) {
+				  		System.out.println(("MATCH " + i + "_" + j));
+				  		set.add(String.valueOf(text.charAt(i)));
+				  		break;
+				  }
+					  
+			  }
+			  
+		  }
+		  
+		  System.out.println("# of duplicate letters: " + set.size());
+		  return set.size();
 	  }
 }
