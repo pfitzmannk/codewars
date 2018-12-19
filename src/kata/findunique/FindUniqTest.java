b4ab999a817594394b7a03c938409ad59206b4d4
Kai Pfitzmann <kai.pfitzmann@exorbyte.com>
2018-12-19 15:52:47 +0100

    Initial Commit for new Kata

---
+++ src/kata/findunique/FindUniqTest.java
@@ -0,0 +1,16 @@
+package kata.findunique;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+// TODO: Replace examples and use TDD development by writing your own tests
+
+public class FindUniqTest {
+    private double precision = 0.0000000000001;
+    
+    @Test
+    public void sampleTestCases() {
+        assertEquals(1.0, KataUnique.findUniq(new double[]{0, 1, 0}), precision);
+        assertEquals(2.0, KataUnique.findUniq(new double[]{1, 1, 1, 2, 1, 1}), precision);
+    }
+}
---
+++ src/kata/findunique/KataUnique.java
@@ -0,0 +1,9 @@
+package kata.findunique;
+
+
+public class KataUnique {
+ public static double findUniq(double arr[]) {
+   // Do the magic
+   return arr[0];
+ }
+}
