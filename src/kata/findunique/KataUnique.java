41df32b2b66991ec6fcbd709e3a3687765fb5db0
Kai Pfitzmann <kai.pfitzmann@exorbyte.com>
2020-08-13 10:31:48 +0200

    Began working on solution. Commit-Test

--- src/kata/findunique/KataUnique.java
+++ src/kata/findunique/KataUnique.java
@@ -1,9 +1,26 @@
 package kata.findunique;
 
 
+/*
+ * There is an array with some numbers. All numbers are equal except for one. Try to find it!
+ * Kata.findUniq(new double[]{ 1, 1, 1, 2, 1, 1 }); // => 2
+ * Kata.findUniq(new double[]{ 0, 0, 0.55, 0, 0 }); // => 0.55
+ * It’s guaranteed that array contains more than 3 numbers.
+ */
+
 public class KataUnique {
- public static double findUniq(double arr[]) {
-   // Do the magic
-   return arr[0];
- }
+ @SuppressWarnings("null")
+	public static Double findUniq(double arr[]) {
+	 
+	 for (int i=0; i<arr.length; i++) {
+		 if (!(arr[i] == arr[i+1])) {
+			 
+		 }
+	 }
+	 
+	 
+
+			 return null;
+	 }
 }
+
